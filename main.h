21a520af6e6ff99e470989a898c4a36b4b5c85a1
Daviddjin <138932352+Daviddjin@users.noreply.github.com>
2023-09-17 11:17:56 +0000

    Update main.h

--- main.h
+++ main.h
@@ -6,6 +6,21 @@
 #include <uninstd.h>
 #include <stddef.h>
 
+/* MACRO DEFINITIONS */
+
+/*  FLAGS MACRO DEFINTION  */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 1
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* SIZE MACRO DEFINITION */
+#define S_LONG 2
+#define S_SHORT 1
 
 int _printf(const char *format, ...);
 
