10135148e14f9abbc34159ac80adb7e8a611c22a
daviddjin <djindavid9@gmail.com>
2023-09-19 14:06:57 -0700

    main header file

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <limits.h>
@@ -8,22 +8,7 @@
 #include <unistd.h>
 #include <string.h>
 
-/* MACRO DEFINITIONS */
-
-/*  FLAGS MACRO DEFINTION  */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 1
-
-#define UNUSED(x) (void)(x)
-#define BUFF_SIZE 1024
-
-/* SIZE MACRO DEFINITION */
-#define S_LONG 2
-#define S_SHORT 1
-
+int _write_char(char c)
 int _printf(const char *format, ...);
 
 #endif
