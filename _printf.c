ed7d47f46831e5f2bfd12b07966608b1f1222fc1
daviddjin <djindavid9@gmail.com>
2023-09-19 03:35:51 -0700

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,25 +1,68 @@
 #include "main.h"
 
-void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - creating a new printf function
- * @format: format char
- * Return: Print char
+ * _printf - printf functoon but handles conversion specifiers
+ * @format: format string.
+ * @...: number of arguments.
+ * Return: prints character
  */
 
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	va_list list;
-	
-	if (format == NULL)
-		return (-1);
+	va_list args;
+	int i, printed_chars = 0;
+	int length = 0;
+	int j = length - 1;
 
-	va_start(list, format);
+	va_start(args, format);
 
-	for (i = 0; format && format[i] != '\0'; i++)
+	while (format && format[i])
 	{
-		if (format[i] != '%')
+		if (format[i] == '%' && (format[i + 1] == 'd' || format[i + 1] == 'i'))
+		{
+			int num = va_arg(args, int);
+			char buffer[20];
+			int length = 0;
+
+			if (num == 0)
+			{
+				buffer[length++] = '0';
+			}
+			else
+			{
+				if (num < 0)
+				{
+					write(1, "-", 1);
+					num = -num;
+					printed_chars++;
+				}
+				while (num > 0)
+				{
+					buffer[length++] = num % 10 + '0';
+						num /= 10;
+				}
+
+				while (j >= 0)
+				{
+					j--;
+				}
+
+				{
+					write(1, &buffer[j], 1);
+					printed_chars++;
+				}
+			}
+
+			i += 2;
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			printed_chars++;
+			i++;
+		}
 	}
-		
+
+	va_end(args);
+	return (printed_chars);
+}
