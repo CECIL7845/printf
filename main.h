52099922656976f73b367fddcfb0be8d65af5b39
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-09-21 05:17:08 -0700

    printf

--- main.h
+++ main.h
@@ -1,17 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <limits.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <string.h>
 
-<<<<<<< HEAD
-=======
-int _write_char(char c);
->>>>>>> refs/remotes/origin/master
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
+int _write_char(char c);
+int _write_str(char *str);
 
 #endif
