9772465db1cb9bd09928fbcfd15b470fa8d6b09f
daviddjin <djindavid9@gmail.com>
2023-09-21 08:47:55 -0700

    update on printf

--- printf.c
+++ printf.c
@@ -3,9 +3,9 @@
 /**
  * _write_char - Writes a character to stdout
  * @c: The character to write
- *
  * Return: The number of characters written (1)
  */
+
 int _write_char(char c)
 {
 	return (write(1, &c, 1));
@@ -14,14 +14,14 @@ int _write_char(char c)
 /**
  * _write_str - Writes a string to stdout
  * @str: The string to write
- *
  * Return: The number of characters written
  */
+
 int _write_str(char *str)
 {
 	int len = 0;
 
-	if (str)
+	if (str != NULL)
 	{
 		len = strlen(str);
 
@@ -38,9 +38,9 @@ int _write_str(char *str)
 /**
  * _printf - Produces output according to a format
  * @format: Pointer to a format string
- *
  * Return: The number of characters printed (excluding null byte)
  */
+
 int _printf(const char *format, ...)
 {
 	int prn = 0;
