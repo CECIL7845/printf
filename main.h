776f1add7ad9777cc881679b85866ee4d166b108
Cecil Odonkor <odonkorcecil123@gmail.com>
2023-09-22 01:57:05 -0700

    printf

--- main.h
+++ main.h
@@ -7,7 +7,7 @@
 
 int _printf(const char *format, ...);
 int _write_char(char c);
-int _write_str(char *str);
+int _write_str(const char *str);
 int handle_format_specifier(const char *format, va_list args);
 
 #endif
--- printf.c
+++ printf.c
@@ -3,9 +3,9 @@
 /**
  * _write_char - Writes a character to stdout
  * @c: The character to write
+ *
  * Return: The number of characters written (1)
  */
-
 int _write_char(char c)
 {
 	return (write(1, &c, 1));
@@ -14,33 +14,27 @@ int _write_char(char c)
 /**
  * _write_str - Writes a string to stdout
  * @str: The string to write
+ *
  * Return: The number of characters written
  */
-
-int _write_str(char *str)
+int _write_str(const char *str)
 {
-	int len = 0;
-
 	if (str != NULL)
 	{
-		len = strlen(str);
-
-		write(1, str, len);
+		return (write(1, str, strlen(str)));
 	}
 	else
 	{
-		write(1, "(null)", len);
+		return (write(1, "(null)", 6));
 	}
-	return (len);
 }
 
 /**
  * _printf - a function that produces output according to a format
  * @format: Pointer to a format string
  *
- * Return: number of characters to be printed
+ * Return: Number of characters to be printed
  */
-
 int _printf(const char *format, ...)
 {
 	int prn = 0;
@@ -52,13 +46,16 @@ int _printf(const char *format, ...)
 	}
 
 	va_start(args, format);
+
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
 			if (*format == '\0')
-			break;
+			{
+				break;
+			}
 
 			prn += handle_format_specifier(format, args);
 		}
@@ -78,38 +75,46 @@ int _printf(const char *format, ...)
  * @format: Pointer to the format string
  * @args: Argument list
  *
- * Return: The number of characters printed
+ * Return: number of characters to be printed
  */
 int handle_format_specifier(const char *format, va_list args)
 {
 	int prn = 0;
 
-	if (*format == 'c')
+	switch (*format)
 	{
-	int c = va_arg(args, int);
-		prn += _write_char(c);
-	}
-	else if (*format == 's')
-	{
-		char *str = va_arg(args, char *);
+		case 'c':
+		{
+			int c = va_arg(args, int);
 
-		if (str == NULL)
-		{
-			prn += _write_str("(null)");
+			prn += _write_char(c);
+			break;
 		}
-		else
+		case 's':
 		{
-			prn += _write_str(str);
+			const char *str = va_arg(args, const char *);
+
+			if (str == NULL)
+			{
+				prn += _write_str("(null)");
+			}
+			else
+			{
+				prn += _write_str(str);
+			}
+			break;
 		}
-	}
-	else if (*format == '%')
-	{
-		prn += _write_char('%');
-	}
-	else
-	{
-		prn += _write_char('%');
-		prn += _write_char(*format);
+		case '%':
+		{
+			prn += _write_char('%');
+			break;
+		}
+		default:
+		{
+			prn += _write_char('%');
+			prn += _write_char(*format);
+			break;
+		}
 	}
 
 	return (prn);
